da32a7dda4ad5e2fe60118c42d0e09e73530bf62
Apekshaj04 <apekshajamjar4@gmail.com>
2024-01-16 10:03:40 +0530

    initital commit

---
+++ app/(clerk)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClerkLayout = ({children}:{children: React.ReactNode}) => {
+  return (
+    <div className='flex justify-center items-center w-full h-full'>
+      {children} 
+    </div>
+  )
+}
+
+export default ClerkLayout
